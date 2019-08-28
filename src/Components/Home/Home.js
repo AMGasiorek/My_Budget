08b0ea99c85e80d1aaf412782bb7a5a2ea95c4e6
AMGasiorek <a.m.gasiorek@gmail.com>
2019-08-28 19:31:33 +0200

    Authentication is working

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,13 @@
-import React, {Component} from 'react';
+import React from 'react';
+import { withAuthorization } from '../Session/Session';
 
-class Home extends Component {
-    render() {
-        return (
-            <p>Home</p>
-        )
-    }
-}
+const HomePage = () => (
+    <div>
+        <h1>Home Page</h1>
+        <p>The Home Page is accessible by every signed in user.</p>
+    </div>
+);
 
-export default Home
+const condition = authUser => !!authUser;
+
+export default withAuthorization(condition)(HomePage);
